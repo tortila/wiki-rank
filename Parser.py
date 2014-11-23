42883c9243051e97d348335857013e51916f6c1e
tortila <g.oliwia@gmail.com>
2014-11-23 21:07:30 +0100

    Parser fixed

---
+++ Parser.py
@@ -0,0 +1,54 @@
+__author__ = 'oliwia'
+
+import os.path
+import numpy as np
+import csv
+
+DIR = "data/"
+LINKS = "Links_"
+TITLES = "Titles_"
+FILE_TYPE = ".txt"
+
+class Parser:
+
+    def __init__(self, suffix):
+        self.links_file = DIR + LINKS + suffix + FILE_TYPE
+        self.titles_file = DIR + TITLES + suffix + FILE_TYPE
+        self.links_size = self.get_links_size()
+        self.links = self.link_read()
+        self.titles = self.title_read()
+        print "Parser: got", self.links_size, "links along with", self.titles_size, "titles."
+
+    def get_links_size(self):
+        max_val = 0
+        with open(self.links_file, "rb") as csv_file:
+            f = csv.reader(csv_file, delimiter=" ", quotechar='|')
+            for line in f:
+                if int(max(line)) > max_val:
+                    max_val = int(max(line))
+        csv_file.close()
+        return max_val
+
+    def link_read(self):
+        size = (int(self.links_size), int(self.links_size))
+        data = np.zeros(size, dtype = np.int)
+        rows = []
+        cols = []
+        with open(self.links_file, "rb") as csv_file:
+            f = csv.reader(csv_file, delimiter=" ", quotechar='|')
+            for line in f:
+                rows.append(int(line[0]) - 1)
+                cols.append(int(line[1]) - 1)
+        csv_file.close()
+        data[rows, cols] = 1
+        return data
+
+    def title_read(self):
+        data = []
+        with open(self.titles_file, "rb") as csv_file:
+            f = csv.reader(csv_file, delimiter=" ", quotechar='|')
+            for line in f:
+                data.append(line)
+        csv_file.close()
+        self.titles_size = len(data)
+        return data
---
+++ wiki-rank.py
@@ -0,0 +1,13 @@
+from Parser import Parser
+
+ALPHA = 0.85
+TEST = "Test"
+REAL_DATA = "oligo854"
+
+def main():
+    print "Welcome to wiki-rank!"
+    parser = Parser(TEST)
+
+
+if __name__ == "__main__":
+    main()
